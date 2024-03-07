b456380f906d7faf544f4cd0514716b414fabc39
Cormac Stewart <cormac.stewart@dear.life>
2024-03-06 22:40:37 -0400

    Initializing project and creating 4 different practice folder management scripts

---
+++ src/scripts/convert-files.ts
@@ -0,0 +1,30 @@
+import {
+  loadAllFiles,
+  print,
+  convertWavToMp3,
+  hasBeenConverted,
+} from "../utils";
+
+export const convertFiles = async () => {
+  try {
+    const files = await loadAllFiles();
+    const recentUnconvertedFiles = files
+      .filter(
+        (f) => f.fileExtension === "wav" && !hasBeenConverted(files, f.mixName)
+      )
+      .slice(0, 5);
+    if (recentUnconvertedFiles.length === 0) {
+      print("No unconverted files found.");
+      return;
+    }
+    print(recentUnconvertedFiles.length + " unconverted wav files found.\n");
+    await Promise.all(
+      recentUnconvertedFiles.map(async (f) => {
+        await convertWavToMp3(f);
+      })
+    );
+  } catch (error) {
+    print(error);
+  }
+};
+convertFiles();
---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export type RenameFileData = {
+  file: FileData;
+  newName: string;
+};
+
+export class FileData {
+  filePath: string;
+  createdAt: Date;
+
+  constructor(filePath: string, createdAt: Date) {
+    this.filePath = filePath;
+    this.createdAt = createdAt;
+  }
+
+  get fileExtension() {
+    const i = this.filePath.lastIndexOf(".");
+    return this.filePath.substring(i + 1, this.filePath.length);
+  }
+
+  get fileName() {
+    const i = this.filePath.lastIndexOf(".");
+    return this.filePath.substring(0, i);
+  }
+
+  get mixName() {
+    return this.filePath.split(".")[0];
+  }
+
+  toString() {
+    return `${this.filePath} ${this.createdAt.toISOString()}`;
+  }
+}
